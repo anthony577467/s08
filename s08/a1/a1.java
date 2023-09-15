1063ddcbd1e3d8ef105aaea24d1720fc65ab7ce3
anthony577467 <136906838+anthony577467@users.noreply.github.com>
2023-09-15 09:23:13 -0500

    Add files via upload

---
+++ s08/a1/a1.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package s08.a1;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class a1 {
+    private int atributoPrivado;
+
+    public a1 (int atributoPrivado) {
+        this.atributoPrivado = atributoPrivado;
+    }
+
+    public int obtenerAtributoPrivado() {
+        return atributoPrivado;
+    }
+}
+
